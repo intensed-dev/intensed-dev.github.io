eed229cdb152e1dac70e02387156e4341f328c9d
Intensed <intensed01@gmail.com>
2026-08-16 16:43:39 +0200

    Add footer HTML structure to main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
-
+const footer = `
+<footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+  <nav>
+    <h6 class="footer-title">Sites</h6>
+    <a class="link link-hover">Home</a>
+    <a class="link link-hover">Links</a>
+    <a class="link link-hover">???</a>
+    <a class="link link-hover">???</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Projects</h6>
+    <a class="link link-hover">???</a>
+    <a class="link link-hover">???</a>
+    <a class="link link-hover">???</a>
+    <a class="link link-hover">???</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">???</h6>
+    <a class="link link-hover">???</a>
+    <a class="link link-hover">???</a>
+    <a class="link link-hover">???</a>
+  </nav>
+</footer>
+<footer class="footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
+  <aside class="grid-flow-col items-center">
+    <i class="fa-solid fa-hashtag"></i>
+    <p>
+      Intensed-Dev
+      <br />
+      Made with ❤️ by myself.
+    </p>
+  </aside>
+  <nav class="md:place-self-center md:justify-self-end">
+    <div class="grid grid-flow-col gap-4">
+      <a href="https://github.com/Intensed-Dev"><i class="fa-brands fa-github"></i></a>
+      <a>
+        
+      </a>
+      <a>
+        
+      </a>
+    </div>
+  </nav>
+</footer>
+`;
