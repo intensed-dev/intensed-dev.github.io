911591091e02e5352eef8b0a5b83f02228928162
Intensed <intensed01@gmail.com>
2026-08-16 17:22:30 +0200

    Refactor footer structure and update links

--- src/main.js
+++ src/main.js
@@ -1,45 +1,42 @@
 const footer = `
 <footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+  <aside>
+    <svg
+      width="50"
+      height="50"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      class="fill-current">
+      <path
+        d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+    </svg>
+    <p>
+      <span class="title">Intensed-Dev</span>
+      <br />
+      Made with ❤️ by myself.
+    </p>
+  </aside>
   <nav>
-    <h6 class="footer-title">Sites</h6>
+    <h6 class="footer-title title">Projects</h6>
+    <a class="link link-hover">KeyframeCSS</a>
+    <a class="link link-hover">Devinsbadges-Customs</a>
+    <a class="link link-hover">???</a>
+    <a class="link link-hover">???</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title title">Sites</h6>
     <a class="link link-hover">Home</a>
     <a class="link link-hover">Links</a>
     <a class="link link-hover">???</a>
     <a class="link link-hover">???</a>
   </nav>
   <nav>
-    <h6 class="footer-title">Projects</h6>
-    <a class="link link-hover">???</a>
-    <a class="link link-hover">???</a>
-    <a class="link link-hover">???</a>
-    <a class="link link-hover">???</a>
-  </nav>
-  <nav>
-    <h6 class="footer-title">???</h6>
-    <a class="link link-hover">???</a>
-    <a class="link link-hover">???</a>
-    <a class="link link-hover">???</a>
-  </nav>
-</footer>
-<footer class="footer bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
-  <aside class="grid-flow-col items-center">
-    <i class="fa-solid fa-hashtag"></i>
-    <p>
-      Intensed-Dev
-      <br />
-      Made with ❤️ by myself.
-    </p>
-  </aside>
-  <nav class="md:place-self-center md:justify-self-end">
-    <div class="grid grid-flow-col gap-4">
-      <a href="https://github.com/Intensed-Dev"><i class="fa-brands fa-github"></i></a>
-      <a>
-        
-      </a>
-      <a>
-        
-      </a>
-    </div>
+    <h6 class="footer-title title">Social</h6>
+    <a class="link link-hover">GitHub</a>
+    <a class="link link-hover">Modrinth</a>
+    <a class="link link-hover">NRC</a>
   </nav>
 </footer>
 `;
