990fccb85dbb3f5111df31ce6b472d02d9beb7d6
Intensed <intensed01@gmail.com>
2026-08-22 08:27:58 +0200

    Update navbar button and search input
    Replaced button text and updated search input structure.

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ const header = `
       <label for="navbar-1-toggle" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <button class="btn btn-ghost text-xl">Intensed-Dev</button>
+      <button class="btn btn-ghost text-xl">daisyUI</button>
     </div>
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
@@ -68,12 +68,7 @@ const header = `
       </ul>
     </div>
     <div class="navbar-end">
-      <label class="input">
-        <i class="fa-solid fa-magnifying-glass"></i>
-        <input type="search" class="grow" placeholder="Search" />
-        <kbd class="kbd kbd-sm">⌘</kbd>
-        <kbd class="kbd kbd-sm">K</kbd>
-      </label>
+      <input type="text" placeholder="Search" class="input w-64 lg:w-auto" />
     </div>
   </div>
 
